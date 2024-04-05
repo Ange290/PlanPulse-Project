bd186e902f358f47d6a77c8dcd589fe865740c92
Ange290 <uange209@gmail.com>
2024-04-05 10:40:18 +0200

    Connect Database

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
-
-mongoose.connect
+dotenv.config();
+const port = process.env.PORT
+const db = process.env.MONGOOSE_URL;
+mongoose.connect(db)
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`Server is live on port ${port}!!`);
+    })
+})
+.catch((err)=>{
+   console.error(err);
+})
