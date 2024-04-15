088a65f0a1bac0a7ed44c91e6807292544adfb7a
Ange290 <uange209@gmail.com>
2024-04-15 10:50:24 +0200

    Update Feature

--- models/task_model.js
+++ models/task_model.js
@@ -4,7 +4,9 @@ const Task_Model = new mongoose.Schema({
     tittle:{
         type:String,
         required: true,
-        unique:[true, "Tittle must be unique"]
+        unique:[true, "Tittle must be unique"],
+        minLength:[5, "Tittle must be at least 5 long" ],
+        mixLength:[50, "Tittle can't exceed 50 characters"]
     },
    description: String,
 
@@ -40,6 +42,21 @@ durationType:{
         message:"{VALUE} is not  a valid duration type"
     }
 }}
+});
+Task_Model.pre('save', function(next){
+    if(this.dueDate.startDate && this.dueDate.endDate){
+        if(this.dueDate.endDate < this.dueDate.startDate){
+            return next(new Error("End Date must be greater than Start Date"));
+        }
+    }
+    if(this.dueDate.startTime && this.dueDate.endTime){
+const startDate = new Date(this.dueDate.startDate);
+const startTime = new Date(startDate.toDateString() + ' ' + this.dueDate.startTime);
+const endTime = new Date(endDate.toDateString() + ' ' + this.dueDate.endTime);
+if(endTime < startTime){
+     return next(new Error("End Time must be greater than Start Time"));
+    }
+}
 });
 Task_Model.path("tittle").validate({
     validator:(value)=>{
